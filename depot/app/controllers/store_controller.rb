58518cbf1d8b2e78d3810d80131ce8639fa6d92f
jbennett7 <jbennett@redhat.com>
2014-11-15 13:42:34 -0500

    Play Time for Chapter 9. Task D: Cart Creation
    See https://pragprog.com/wikis/wiki/Task-D-CartCreation
    
    * Add a new variable to the session to record how many times the user has accessed the store controller's index action.  Note that the first time this page is accessed, your cont won't be in the session.  You can test for this with code like this:
    
      if session[:counter].nil?
        ...
      end
    
    * Pass this counter to your template, and display it at the top of the catalog page.  Hint: the pluralize helper might be useful when forming hte message you display.
    
    * Reset the counter to zero whenever the user adds something to the cart.
    
    * Change the template to display the counter only if it is greater than five.

--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -2,5 +2,11 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     @date_time = Time.now
+    @count = increment_count
+  end
+
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 end
