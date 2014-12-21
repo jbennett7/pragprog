dbbfdedd4cfea6e9b7beceb6cb81c733b79d0861
jbennett7 <jbennett@redhat.com>
2014-12-21 12:11:38 -0500

    Changes to be committed:  	modified:   depot/test/models/cart_test.rb  	new file:   history/history-201412211209.txt  	new file:   journal/journal-201412211209.txt
     Add unit tests that add unique products and duplicate products.  Note that you will need to modify the fixture to refer to products and carts by name, for example product: ruby.

--- depot/test/models/cart_test.rb
+++ depot/test/models/cart_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def add_a_product(cart, product_name)
+    cart.add_product(products(product_name).id, products(product_name).price)
+    cart
+  end
+
+  test "add two different products" do
+    cart = Cart.create
+    cart = add_a_product(cart,:one)
+    cart = add_a_product(cart,:two)
+    cart.save
+    compare_price = products(:one).price + products(:two).price
+    assert_equal 2, cart.line_items.count
+    assert_equal compare_price, cart.total_price
+  end
+
+  test "add duplicate products" do
+    cart = Cart.create
+    cart = add_a_product(cart,:one)
+    cart = add_a_product(cart,:one)
+    cart.save
+    assert_equal 2, cart.line_items.count
+    assert_equal 2*products(:one).price, cart.total_price
+  end
 end
